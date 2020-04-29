f46c7cb8c19270a33fbfa61f4876e6dc0386b63d
Leandro | Gnosis <leandroboscariol@gmail.com>
2020-04-29 10:48:54 -0700

    Adjusting case on DXTokenRegistry imports

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
 const Migrations = artifacts.require("Migrations");
-const DxTokenRegistry = artifacts.require("dxTokenRegistry");
+const DxTokenRegistry = artifacts.require("DXTokenRegistry");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
